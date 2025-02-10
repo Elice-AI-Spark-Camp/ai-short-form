5f2d1b309996dc350c0fcda7973bda495f4c41e1
YachaTree <dnftksghkfkd@icloud.com>
2025-02-10 14:55:37 +0900

    Feat: 프로젝트 셋팅

---
+++ src/main/java/elice/aishortform/AiShortFormApplication.java
@@ -0,0 +1,13 @@
+package elice.aishortform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiShortFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiShortFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elice/aishortform/AiShortFormApplicationTests.java
@@ -0,0 +1,13 @@
+package elice.aishortform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiShortFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
