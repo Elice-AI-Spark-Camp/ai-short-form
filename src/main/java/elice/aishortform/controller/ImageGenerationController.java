d6d9e5dc8f45464bdd3e68b775d49f23ca21b71b
yujin-zero <poly30@naver.com>
2025-02-18 12:56:00 +0900

    Feat: Swagger 문서화 추가 (ImageAPI)
    - ImageApiDocs 인터페이스를 생성하여 API 문서화
    - /generate-all 엔드포인트 설명 추가

--- src/main/java/elice/aishortform/controller/ImageGenerationController.java
+++ src/main/java/elice/aishortform/controller/ImageGenerationController.java
@@ -4,22 +4,23 @@ import elice.aishortform.dto.ImageGenerationRequestDto;
 import elice.aishortform.dto.ImageGenerationResponseDto;
 import elice.aishortform.dto.ImageGenerationResponseDto.ImageDto;
 import elice.aishortform.service.ImageGenerationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(name = "Image API", description = "이미지 생성 및 조회 API")
 @RestController
 @RequestMapping("/images")
 @RequiredArgsConstructor
-public class ImageGenerationController {
+public class ImageGenerationController implements ImageApiDocs {
 
     private final ImageGenerationService imageGenerationService;
 
-    @PostMapping("/generate-all")
     public ResponseEntity<ImageGenerationResponseDto> generateImages(@RequestBody ImageGenerationRequestDto request) {
         List<ImageDto> images = imageGenerationService.generateImages(request.summaryId());
         return ResponseEntity.ok(new ImageGenerationResponseDto(images, images.size()));
