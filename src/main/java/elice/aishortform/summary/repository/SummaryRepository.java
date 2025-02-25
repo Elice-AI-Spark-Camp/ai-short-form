bd34b3103ca6cb32944afa4c4a1d6d6dd909f1be
YachaTree <dnftksghkfkd@icloud.com>
2025-02-25 16:38:55 +0900

    Fix: 지연 로딩 문제를 해결

--- src/main/java/elice/aishortform/summary/repository/SummaryRepository.java
+++ src/main/java/elice/aishortform/summary/repository/SummaryRepository.java
@@ -5,10 +5,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface SummaryRepository extends JpaRepository<Summary, Long> {
     Optional<Summary> findBySummaryId(Long summaryId);
 
-    @Query("SELECT s FROM Summary s LEFT JOIN FETCH s.paragraphImageMap WHERE s.summaryId = :summaryId")
-    Optional<Summary> findByIdWithParagraphImageMap(@Param("summaryId") Long summaryId);
+    @Query("SELECT s FROM Summary s LEFT JOIN FETCH s.paragraphImageMap LEFT JOIN FETCH s.paragraphs WHERE s.summaryId = :summaryId")
+    Optional<Summary> findByIdWithParagraphImageMap(@PathVariable Long summaryId);
 }
